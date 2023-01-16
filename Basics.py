02396f5bd6a90a098fac15e54cf784c506466c65
Hariharan_Hark <113651469+Hariharan-L@users.noreply.github.com>
2023-01-16 10:28:52 +0530

    Add files via upload

---
+++ Basics.py
@@ -0,0 +1,72 @@
+# r=int(input("Enter the radius:"))#diameter of the sphere
+# volume = (4/3)*3.14*r**3
+# # vol=int(volume)
+# print(int(volume))
+# # print(volume)
+# import pandas
+# r=dir(pandas)
+# print(r)
+
+# str="DasdFNJadlclmscnljjkjdkKkjdkJJJJKJK"
+# s=str.upper()
+# print(s)
+
+# str="DasdFNJadlclmscnljjkjdkKkjdkJJJJKJK"
+# d=str.lower()
+# print(d)
+
+# str="DasdFNJadlclmscnljjkjdkKkjdkJJJJKJK"
+# j=str.capitalize()
+# print(j)
+
+# str="DasdFNJadlclmscnljjkjdkKkjdkJJJJKJK"
+# le=str.swapcase()
+# print(le)
+#### arithematic opeateros ####
+# exponent == **
+# parantheses == ()
+# sub,add,mult,div == -,+,*,/
+
+######## assignment operator ########
+
+# equal to = 
+# *=
+# +=
+# -=
+# /=
+# power  **=
+
+
+######## relational/comparision operator ########
+# less than  <
+# greater than  >
+# >=
+# <=
+# == condition equal to or not
+# !=  not equal to
+
+######## logical operator ########
+
+# or     logical OR
+# and    logical AND
+# not    logical NOT
+
+######## bitwise operator ########
+# or |
+# and &
+
+######## operators #########
+# floor division // its give the value with .0 valu round off
+
+
+
+
+# # 011
+# # 010
+# a=20.5
+# b=3
+# # print(a and b)
+# p=5.0
+# print(a//b)
+
+
